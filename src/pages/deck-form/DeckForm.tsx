d085cfd75a310215bb9f7dc86170e0dcb2350f3f
Paulo Henrique <pauloh1288@gmail.com>
2020-07-11 20:12:04 -0300

    chore: makes improvements in the form validation and submit function

--- src/pages/deck-form/DeckForm.tsx
+++ src/pages/deck-form/DeckForm.tsx
@@ -1,40 +1,90 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import { Formik, Form, Field, FieldProps, FormikErrors } from 'formik';
+import { Formik, Form, Field, FormikErrors, ErrorMessage } from 'formik';
 import { cards, initialCards } from './DeckForm.content';
+import { createDeck, createPile } from '../../services/api';
+import {
+  CARDS_PILE_NAME,
+  ROTATION_CARD_PILE_NAME,
+} from '../../utils/constants';
 
 type DeckFormProps = RouteComponentProps;
 type FormValues = { cards: string[]; rotationCard: string };
 
+function validateCardRequirements(cards: string[]) {
+  let hasError = true;
+
+  cards.forEach(card => {
+    if (card) {
+      hasError = false;
+    }
+  });
+
+  return hasError;
+}
+
 export default function DeckForm(props: DeckFormProps) {
+  const [hasMinimalCardsError, setHasMinimalCardsError] = React.useState(
+    false,
+  );
+
   function onValidate(values: FormValues): FormikErrors<FormValues> {
     const errors = { cards: initialCards, rotationCard: '' };
+    let hasError = false;
 
     values.cards.forEach((card, index) => {
       if (card && cards.indexOf(card) === -1) {
         errors.cards[index] = `${card} não é uma carta válida`;
+        hasError = true;
       } else {
         errors.cards[index] = '';
       }
     });
 
-    if (values.rotationCard && cards.indexOf(values.rotationCard) === -1) {
-      errors.rotationCard = `${values.rotationCard} não é uma carta válida`;
+    if (values.rotationCard) {
+      if (cards.indexOf(values.rotationCard) === -1) {
+        errors.rotationCard = `${values.rotationCard} não é uma carta válida`;
+        hasError = true;
+      } else {
+        errors.rotationCard = '';
+      }
     } else {
-      errors.rotationCard = '';
+      errors.rotationCard = 'Rotation card is required';
+      hasError = true;
     }
 
-    return errors;
+    if (hasMinimalCardsError) {
+      setHasMinimalCardsError(false);
+    }
+
+    return hasError ? errors : {};
   }
 
-  function onSubmit(values: FormValues) {
-    console.log(values);
-    props.history.push('/deck/123');
+  async function onSubmit(values: FormValues) {
+    const isInvalid = validateCardRequirements(values.cards);
+
+    if (isInvalid) {
+      setHasMinimalCardsError(true);
+      return;
+    }
+
+    try {
+      const data = await createDeck();
+      await createPile(data.deck_id, CARDS_PILE_NAME, values.cards);
+      await createPile(data.deck_id, ROTATION_CARD_PILE_NAME, [
+        values.rotationCard,
+      ]);
+
+      props.history.push(`/deck/${data.deck_id}`);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
     <div>
       Cards
+      {hasMinimalCardsError && <div>Cards has a minimal quantity</div>}
       <Formik
         initialValues={{ cards: initialCards, rotationCard: '' }}
         validate={onValidate}
@@ -43,34 +93,14 @@ export default function DeckForm(props: DeckFormProps) {
         {formikBag => (
           <Form>
             {formikBag.values.cards.map((_, index) => (
-              <Field key={`card-${index}`} name={`cards[${index}]`}>
-                {({ field, meta }: FieldProps) => (
-                  <div>
-                    <label>
-                      <input type="text" {...field} />
-                    </label>
-                    {meta.touched && meta.error && (
-                      <div data-testid={`card-${index}-error`}>
-                        {meta.error}
-                      </div>
-                    )}
-                  </div>
-                )}
-              </Field>
+              <div key={`card-${index}`}>
+                <Field name={`cards[${index}]`} />
+                <ErrorMessage name={`cards[${index}]`} />
+              </div>
             ))}
-            <Field name="rotationCard">
-              {({ field, meta }: FieldProps) => (
-                <div>
-                  <label htmlFor="rotationCard">Rotation Card</label>
-                  <input id="rotationCard" type="text" {...field} />
-                  {meta.touched && meta.error && (
-                    <div data-testid="rotation-card-error">
-                      {meta.error}
-                    </div>
-                  )}
-                </div>
-              )}
-            </Field>
+            <label htmlFor="rotationCard">Rotation Card</label>
+            <Field id="rotationCard" name="rotationCard" />
+            <ErrorMessage name="rotationCard" />
             <button type="submit">Submit</button>
           </Form>
         )}
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const CARDS_PILE_NAME = 'cards';
+export const ROTATION_CARD_PILE_NAME = 'rotationCard';
