24ce5b51a8bbd5adec67cf45db935f28dfbf612f
Paulo Henrique <pauloh1288@gmail.com>
2020-07-13 13:31:19 -0300

    feat: implements <PageNotFound/> component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import DeckForm from './pages/deck-form/DeckForm';
 import DeckInfo from './pages/deck-info/DeckInfo';
+import PageNotFound from './pages/page-not-found/PageNotFound';
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         </Route>
         <Route path="/deck/new" component={DeckForm} />
         <Route path="/deck/:deckId" component={DeckInfo} />
+        <Route component={PageNotFound} />
       </Switch>
     </Router>
   );
