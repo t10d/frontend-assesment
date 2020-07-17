cdd9899a6f5afcf709e59922e7189cb51e6f4dba
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-07-18 00:46:57 +0200

    feat: Add deck reducer to store

--- src/app/components/Deck/components/NewDeck.tsx
+++ src/app/components/Deck/components/NewDeck.tsx
@@ -4,9 +4,18 @@ import { Input } from "../../Form";
 import styles from "./Deck.module.css";
 import { validation, sortCards } from "../../../../utils";
 import { createDeck, drawCards, createPile } from "../services";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectDeckInfo,
+  getDeckDetailsSuccess,
+  getDeckDetailsStart,
+  getDeckDetailsError,
+} from "../reducers";
 
 const NewDeck = (props: any) => {
   const { register, handleSubmit, errors } = useForm();
+  const { loading, error } = useSelector(selectDeckInfo);
+  const dispatch = useDispatch();
 
   const onSubmit = (data: any) => {
     const rotationCard = data.rotation;
@@ -14,16 +23,26 @@ const NewDeck = (props: any) => {
 
     const cards = Object.values(data)
       .filter((card: any) => Boolean(card))
-      .map((card: any) => card.toUpperCase());
+      .map((card: any) => {
+        return { code: card.toUpperCase() };
+      });
 
+    dispatch(getDeckDetailsStart());
     createDeck(cards)
       .then((deck) => deck)
       .then((deck) => drawCards(deck.deck_id, deck.remaining))
       .then((deck) => createPile(deck.deck_id, sortCards(cards, rotationCard)))
       .then((result) => {
-        // Set things to redux
+        dispatch(
+          getDeckDetailsSuccess({
+            deck_id: result.deck_id,
+            piles: result.piles,
+            rotation: rotationCard,
+          })
+        );
         props.history.push(`/deck/${result.deck_id}`);
-      });
+      })
+      .catch((error) => dispatch(getDeckDetailsError({ error })));
   };
 
   const RenderError = () => (
@@ -35,104 +54,106 @@ const NewDeck = (props: any) => {
       <header className={styles.header}>
         <h2 className={styles.headerTitle}>Cards</h2>
       </header>
-      <section className={styles.content}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <h3 className={styles.cardsTitle}>Select your cards:</h3>
-          <div className={styles.cardsContainer}>
-            <div>
+      {!loading && !error && (
+        <section className={styles.content}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h3 className={styles.cardsTitle}>Select your cards:</h3>
+            <div className={styles.cardsContainer}>
+              <div>
+                <Input
+                  placeholder="Card 1"
+                  name="card1"
+                  register={register(validation)}
+                />
+                {errors.card1 && <RenderError />}
+              </div>
+              <div>
+                <Input
+                  placeholder="Card 2"
+                  name="card2"
+                  register={register(validation)}
+                />
+                {errors.card2 && <RenderError />}
+              </div>
+              <div>
+                <Input
+                  placeholder="Card 3"
+                  name="card3"
+                  register={register(validation)}
+                />
+                {errors.card3 && <RenderError />}
+              </div>
+              <div>
+                <Input
+                  placeholder="Card 4"
+                  name="card4"
+                  register={register(validation)}
+                />
+                {errors.card4 && <RenderError />}
+              </div>
+              <div>
+                <Input
+                  placeholder="Card 5"
+                  name="card5"
+                  register={register(validation)}
+                />
+                {errors.card5 && <RenderError />}
+              </div>
+              <div>
+                <Input
+                  placeholder="Card 6"
+                  name="card6"
+                  register={register(validation)}
+                />
+                {errors.card6 && <RenderError />}
+              </div>
+              <div>
+                <Input
+                  placeholder="Card 7"
+                  name="card7"
+                  register={register(validation)}
+                />
+                {errors.card7 && <RenderError />}
+              </div>
+              <div>
+                <Input
+                  placeholder="Card 8"
+                  name="card8"
+                  register={register(validation)}
+                />
+                {errors.card8 && <RenderError />}
+              </div>
+              <div>
+                <Input
+                  placeholder="Card 9"
+                  name="card9"
+                  register={register(validation)}
+                />
+                {errors.card9 && <RenderError />}
+              </div>
+              <div>
+                <Input
+                  placeholder="Card 10"
+                  name="card10"
+                  register={register(validation)}
+                />
+                {errors.card10 && <RenderError />}
+              </div>
+            </div>
+            <div className={styles.rotation}>
               <Input
-                placeholder="Card 1"
-                name="card1"
+                placeholder="Rotation Card"
+                name="rotation"
                 register={register(validation)}
               />
-              {errors.card1 && <RenderError />}
+              {errors.rotation && <RenderError />}
             </div>
-            <div>
-              <Input
-                placeholder="Card 2"
-                name="card2"
-                register={register(validation)}
-              />
-              {errors.card2 && <RenderError />}
-            </div>
-            <div>
-              <Input
-                placeholder="Card 3"
-                name="card3"
-                register={register(validation)}
-              />
-              {errors.card3 && <RenderError />}
-            </div>
-            <div>
-              <Input
-                placeholder="Card 4"
-                name="card4"
-                register={register(validation)}
-              />
-              {errors.card4 && <RenderError />}
-            </div>
-            <div>
-              <Input
-                placeholder="Card 5"
-                name="card5"
-                register={register(validation)}
-              />
-              {errors.card5 && <RenderError />}
-            </div>
-            <div>
-              <Input
-                placeholder="Card 6"
-                name="card6"
-                register={register(validation)}
-              />
-              {errors.card6 && <RenderError />}
-            </div>
-            <div>
-              <Input
-                placeholder="Card 7"
-                name="card7"
-                register={register(validation)}
-              />
-              {errors.card7 && <RenderError />}
-            </div>
-            <div>
-              <Input
-                placeholder="Card 8"
-                name="card8"
-                register={register(validation)}
-              />
-              {errors.card8 && <RenderError />}
-            </div>
-            <div>
-              <Input
-                placeholder="Card 9"
-                name="card9"
-                register={register(validation)}
-              />
-              {errors.card9 && <RenderError />}
-            </div>
-            <div>
-              <Input
-                placeholder="Card 10"
-                name="card10"
-                register={register(validation)}
-              />
-              {errors.card10 && <RenderError />}
-            </div>
-          </div>
-          <div className={styles.rotation}>
-            <Input
-              placeholder="Rotation Card"
-              name="rotation"
-              register={register(validation)}
-            />
-            {errors.rotation && <RenderError />}
-          </div>
-          <button type="submit" className={styles.button}>
-            Submit
-          </button>
-        </form>
-      </section>
+            <button type="submit" className={styles.button}>
+              Submit
+            </button>
+          </form>
+        </section>
+      )}
     </div>
   );
 };
--- src/app/components/Deck/reducers/index.ts
+++ src/app/components/Deck/reducers/index.ts
@@ -1,17 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { DeckState } from "./../services/types";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../../store";
 
-const initialState: any = {
-  x: 0,
+const initialState: DeckState = {
+  loading: false,
+  error: null,
+  deck_id: undefined,
+  piles: undefined,
 };
 
-export const newDeckSlice = createSlice({
+export const deckSlice = createSlice({
   name: "deck",
   initialState,
   reducers: {
-    example: (state) => {
-      state.x = 250;
+    getDeckDetailsStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+
+    getDeckDetailsSuccess: (state, action: PayloadAction<DeckState>) => {
+      const { deck_id, piles, rotation } = action.payload;
+      state.loading = false;
+      state.deck_id = deck_id;
+      state.piles = piles;
+      rotation && (state.rotation = rotation);
+    },
+
+    getDeckDetailsError: (state, action: PayloadAction<DeckState>) => {
+      state.loading = false;
+      state.error = action.payload.error;
+    },
+
+    addPilesToDeck: (state, action: PayloadAction<DeckState>) => {
+      const { piles } = action.payload;
+
+      state.piles = { ...state.piles, ...piles };
     },
   },
 });
 
-export const { example } = newDeckSlice.actions;
+export const {
+  getDeckDetailsStart,
+  getDeckDetailsSuccess,
+  getDeckDetailsError,
+  addPilesToDeck,
+} = deckSlice.actions;
+
+export const selectDeckInfo = (state: RootState) => state.deck;
+
+export default deckSlice.reducer;
