8ab9e32d4a29f49615ab42d1aff3a2168e89290d
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-07-17 18:08:15 +0200

    feat: add relevant api calls

--- src/app/components/Deck/services/index.ts
+++ src/app/components/Deck/services/index.ts
@@ -1,15 +1,13 @@
-import axios from "axios";
+import api from "../../../services";
 
-const ax = axios.create({
-  baseURL: "https://...",
-});
-
-export const createDeck = (cards: any): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    resolve();
-  });
+export const createDeck = (cards: string[]): Promise<any> => {
+  return api.get(`/new/?cards=${cards.join()}`).then(({data}) => data);
 };
 
-export const shuffleDeck = (): Promise<any> => {
-  return new Promise((resolve, reject) => resolve());
+export const drawCards = (deckID: string, count: number): Promise<any> => {
+  return api.get(`/deck/${deckID}/draw/?count=${count}`).then(({data}) => data);
 };
+
+export const createPile = (deckID: string, pileName: string, cards: string[]) => {
+  return api.get(`/deck/${deckID}/pile/${pileName}/add/?cards=${cards.join()}`).then(({data}) => data);
+}
