374c29211d5b51bc146bb5d7157857a2fde86c16
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-07-18 00:45:49 +0200

    feat: Add deck reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import deckReducer from "../app/components/Deck/reducers";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    deck: deckReducer,
   },
 });
 
