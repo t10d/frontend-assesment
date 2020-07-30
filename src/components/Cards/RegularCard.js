34e3687973721f805ac606e64e4d24b9349f4b71
Diogo Gutierre <diogorg@gmail.com>
2020-07-30 08:48:21 -0300

    fix problem with card 10. Wrong double char value

--- src/components/Cards/RegularCard.js
+++ src/components/Cards/RegularCard.js
@@ -2,16 +2,18 @@ import { Box, Paper } from '@material-ui/core'
 import PropTypes from 'prop-types'
 import React from 'react'
 import suitOrder from 'utils/json/suitOrder.json'
+import valueOrder from 'utils/json/valueOrder.json'
 
 function RegularCard({ card }) {
   const suitObj = suitOrder.find((suit) => suit.value === card.suit)
+  const valueObj = valueOrder.find((v) => v.code === card.value)
   return (
     <Paper style={{ position: 'relative', color: suitObj.color }} className='game-card'>
-      <Box className='card-value1'>{card.value}</Box>
+      <Box className='card-value1'>{valueObj.card}</Box>
       <Box className='suit'>
         <img alt='card' src={`/suits/${card.suit}.png`} />
       </Box>
-      <Box className='card-value2'>{card.value}</Box>
+      <Box className='card-value2'>{valueObj.card}</Box>
     </Paper>
   )
 }
