a40dce1db83af0ddb93dc5d0ecb9825055db9f29
Diogo Gutierre <diogorg@gmail.com>
2020-07-31 14:08:12 -0300

    add mocha testing to test functions

---
+++ mocha/fullhouse.spec.js
@@ -0,0 +1,7 @@
+const chai =  require('chai')
+
+describe('FULLHOUSE FUNCTIONS', () => {
+  it('1. BBBB', (done) => {
+    done()
+  })
+}).timeout(5000)
---
+++ mocha/result.spec.js
@@ -0,0 +1,7 @@
+const chai =  require('chai')
+
+describe('RESULT FUNCTIONS', () => {
+  it('1. AAAA', (done) => {
+    done()
+  })
+}).timeout(5000)
